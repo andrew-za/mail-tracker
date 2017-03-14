b972ec81b4615cd3e4353f0d65f2780219dae870
Andrew Campher <andrew@backbonemedia.co>
2017-03-14 16:27:40 +0200

    Typo

--- database/migrations/2017_03_14_253451_model-morph-relation-table.php
+++ database/migrations/2017_03_14_253451_model-morph-relation-table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddMorphRelationTable extends Migration
+class ModelMorphRelationTable extends Migration
 {
     /**
      * Run the migrations.
